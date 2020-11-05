ecbd4f65dffc2af50e4c0487a52682a22546e809
joshua <javanmega0@gmail.com>
2020-11-05 16:59:11 +0300

    initial commit

---
+++ app/src/main/java/com/adapter/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mygridlayout.R;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Integer> image;
+
+    public MyAdapter(Context context, List<Integer> images) {
+        this.mContext = context;
+        this.image = images;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.grid_item, parent , false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    holder.mImageView.setImageResource(image.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return image.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView mImageView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/mygridlayout/MainActivity.java
@@ -0,0 +1,47 @@
+package com.mygridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.adapter.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+private RecyclerView mRecyclerView;;
+private List<Integer> mImages;
+private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        adapter = new MyAdapter(this,  mImages);
+        mImages = new ArrayList<>();
+
+        mImages.add(R.drawable.one);
+        mImages.add(R.drawable.two);
+        mImages.add(R.drawable.three);
+        mImages.add(R.drawable.four);
+        mImages.add(R.drawable.five);
+        mImages.add(R.drawable.six);
+        mImages.add(R.drawable.seven);
+        mImages.add(R.drawable.eight);
+        mImages.add(R.drawable.nine);
+        mImages.add(R.drawable.ten);
+
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2, GridLayoutManager.VERTICAL,false );
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setAdapter(adapter);
+    }
+}
