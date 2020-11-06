ef75ccbc00070fde2abc4aac2f0a3129fe4ddfc9
joshua <javanmega0@gmail.com>
2020-11-06 10:24:11 +0300

    onClick Listener added

--- app/src/main/java/com/adapter/MyAdapter.java
+++ app/src/main/java/com/adapter/MyAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -49,6 +50,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             super(itemView);
 
             mImageView = itemView.findViewById(R.id.imageView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(), "Downloading ", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
